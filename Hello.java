d12d4be077b2a1f92269e66b463c2b68d3592779
kasireddyt <kasireddit@gmail.com>
2019-01-24 14:10:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public static void main (String[] args )
 {
 for(int i=0;i<=10;i++)
 {
-System.out.println("Hello Welcome to World ........this is chitti" +i);
+System.out.println("Hello Welcome to World ........this is test"+i);
 
 }
 }
